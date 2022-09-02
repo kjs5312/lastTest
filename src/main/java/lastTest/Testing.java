87fe485233deb5ee4f289037a7c9dec3fba28022
Kim Dongseop <Kim Dongseop@DESKTOP-4G6JBS7>
2022-09-02 17:16:31 +0900

    first

---
+++ src/main/java/lastTest/Testing.java
@@ -0,0 +1,11 @@
+package lastTest;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("11111111111111111");
+	}
+
+}
